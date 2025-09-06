54b789895083f47ca88ee5a9323e86a1aa11ba15
din <dineshkjava684@gmail.com>
2025-09-06 02:28:33 -0700

    added logic to count words

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(file_path):
+    result_text = ""
+    try:
+        with open(file_path, encoding='utf-8') as path:
+            result_text = path.read()
+            num_words = result_text.split()
+            print(f"{num_words} words found in the document")
+
+    except Exception as e:
+        print(e)
+
+    return result_text
+
+
+get_book_text("books/frankenstein.txt")
